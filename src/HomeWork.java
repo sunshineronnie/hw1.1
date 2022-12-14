40ecf31a119191c05a0ff0c33290182d49de332b
Admin <kqadb@mail.ru>
2022-12-15 00:26:22 +0500

    git ignore

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,7 +1,17 @@
 public class HomeWork {
     public static void main(String[] args){
 
+
+
+
         System.out.println("Hello, world!");
 
-    }
-}
+                int box = 9;
+                System.out.println(box);
+                int cup = 2;
+                int box1 = box - cup;
+                System.out.println(box1);
+                int map = box + cup;
+        System.out.println(map);
+
+    }}
